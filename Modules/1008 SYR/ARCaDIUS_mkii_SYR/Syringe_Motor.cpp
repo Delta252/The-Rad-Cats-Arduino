c3c56382dab1a363c6dc81ac4a90395ee32c1af6
Troy Lasta <troy.lasta@student.manchester.ac.uk>
2024-04-03 04:10:15 +0100

    Attempted to allow syringe pump firmware to compile on both Nano Every and Uno boards
    Pending testing on hardware.
    Added AFMotor libraries to file directory to reduce dependencies to install.

--- Modules/1008 SYR/ARCaDIUS_mkii_SYR/Syringe_Motor.cpp
+++ Modules/1008 SYR/ARCaDIUS_mkii_SYR/Syringe_Motor.cpp
@@ -1,9 +1,36 @@
 #include "Syringe_Motor.h"
 
-AccelStepper accelMotor(MOTOR_INTERFACE_TYPE, STEP, DIR);
+//Able to compile with both Nano Every and Uno boards with the HW130 Motor Shield.
+#ifdef ARDUINO_NANO_EVERY
+  #define BOARD "NanoEvery"
+  AccelStepper accelMotor(MOTOR_INTERFACE_TYPE, STEP, DIR);
+#endif
+
+//Uno boards with HW130 motor shield uses AFMotor library.
+#ifdef ARDUINO_AVR_UNO
+  #define BOARD "Uno"
+  #include "AFMotor.h"
+  AF_Stepper motor(200, 1);
+
+  //Define forward step and backward step for constructor
+  void forwardstep()
+  {
+    motor.onestep(FORWARD, SINGLE);
+  }
+
+  void backwardstep()
+  {
+    motor.onestep(BACKWARD, SINGLE);
+  }
+
+  //Use custom AccelStepper constructor for use with motor shield (See AccelStepper documentation for more information)
+  AccelStepper accelMotor(forwardstep, backwardstep);
+#endif
 
 void StepperMotor::setUp(void)
 {
+  Serial.println(BOARD);
+
   //Set enable pin to high
   accelMotor.setPinsInverted(false, false, true);
   accelMotor.setEnablePin(ENABLE);
--- Modules/1008 SYR/ARCaDIUS_mkii_SYR/Syringe_Motor.h
+++ Modules/1008 SYR/ARCaDIUS_mkii_SYR/Syringe_Motor.h
@@ -1,12 +1,14 @@
 #include <Arduino.h>
 #include "AccelStepper.h"
 
+
 //Motor interface type: 1 = Stepper driver
 #define MOTOR_INTERFACE_TYPE 1
 //Only change step, dir, or enable if the PCB changes the GPIO pins available to these 
 #define STEP 8
 #define DIR 7
 #define ENABLE 4
+
 
 class StepperMotor
 {
